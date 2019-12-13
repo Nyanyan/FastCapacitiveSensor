776ffbfe3a977c2702ea099779b5d1b8d6a085e3
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-12-13 18:59:19 +0900

    modified: TBD

--- CapacitiveSensor_Nyanyan.cpp
+++ CapacitiveSensor_Nyanyan.cpp
@@ -20,16 +20,16 @@ double CapasitiveSensor::touch(int send, int receive, double voltage) {
   double INPUTTHRESHOLD = voltage * 1024 / 5 * 0.9;
 
   for (int i = 0; i < FREQUENCY; i++) {
-    float val = 0;
-    unsigned long tmp;
+    double val = 0;
+    unsigned long starttim;
     digitalWrite(send, HIGH);
-    tmp = micros();
+    starttim = micros();
     while (analogRead(receive) < INPUTTHRESHOLD) {
-      val = micros() - tmp;
+      val = micros() - starttim;
       if (val > BREAKTHRESHOLD) {
-        int tim = micros() - tmp;
+        int tim = micros() - starttim;
         int vol = analogRead(receive);
-        float gamma = -log(1 - vol / (INPUTTHRESHOLD / 0.9)) / tim;
+        double gamma = -log(1 - vol / (INPUTTHRESHOLD / 0.9)) / tim;
         val = -log(1 - 0.9) / gamma;
         break;
       }
@@ -47,7 +47,7 @@ double CapasitiveSensor::touch(int send, int receive, double voltage) {
   int except = FREQUENCY * EXCEPTRATIO + 1;
   for (int i = except;i < FREQUENCY - except;i++)
     VALsum += VAL[i];
-  int tmp = FREQUENCY - 2 * except;
+  int dev = FREQUENCY - 2 * except;
   //Serial.print(VAL1sum / tmp);
   //Serial.print("\t");
   //Serial.print(VAL2sum / tmp);
@@ -56,5 +56,5 @@ double CapasitiveSensor::touch(int send, int receive, double voltage) {
   //NyanTimer::printLCD(0,1,String(VAL2sum / tmp));
   //NyanTimer::printLCD(7,0,String(threshold1));
   //NyanTimer::printLCD(7,1,String(threshold2));
-  return VALsum / tmp;
+  return VALsum / dev;
 }
